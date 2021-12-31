a2e4dbd5fe5e06058b3d52b987e0c34935874573
elmatus <martin.bergler.es2a@gmail.com>
2021-12-30 22:19:59 -0300

    Mejora en la interpolación por matrices de 3X3

--- educiaa-code/inc/tasks_pressure.h
+++ educiaa-code/inc/tasks_pressure.h
@@ -51,6 +51,7 @@
 
 void set_matrix_index( void* pvParameters );
 void get_pressure_value( void* pvParameters );
+void calculate_arrays( void* pvParameters);
 void interpolate_matrix( void* pvParameters);
 void print_matrix( void* pvParameters );
 
--- educiaa-code/src/tasks_pressure.c
+++ educiaa-code/src/tasks_pressure.c
@@ -10,6 +10,10 @@ QueueHandle_t xSetIndexQueue = NULL;
 QueueHandle_t xPrintQueue = NULL;
 TaskHandle_t TaskHandle_set_matrix_index;
 
+int aux_array_a[(MAX_COL*2)-1] = {};
+int aux_array_b[(MAX_COL*2)-1] = {};
+int aux_array_c[(MAX_COL*2)-1] = {};
+
 void set_matrix_index( void* pvParameters )
 {
 
@@ -23,9 +27,8 @@ void set_matrix_index( void* pvParameters )
     // ---------- REPEAT FOR EVER --------------------------
 	while( TRUE )
 	{
-		if(xQueueReceive( xSetIndexQueue, &( aux_queue ), ( TickType_t ) 10 ) == pdTRUE)
+		if(xQueueReceive( xSetIndexQueue, &( row ), ( TickType_t ) 10 ) == pdTRUE)
 		{
-
 			if(col < MAX_COL-1)
 			{
 				col++;
@@ -48,7 +51,7 @@ void get_pressure_value( void* pvParameters )
     TickType_t xPeriodicity =  1000 / portTICK_RATE_MS;
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
-    int sensor_value = 0;
+    int sensor_value = 1;
 	// ****************************
 	// Sin PCB:
 	#ifdef SENSOR
@@ -72,16 +75,19 @@ void get_pressure_value( void* pvParameters )
 			// Finished to Read all matrix values:
 			if (row >= MAX_ROW)
 			{
+				stdioPrintf(UART_USED, "calculate_arrays create task\n");
 
 				xTaskCreate(
-					interpolate_matrix,                  	// Function of the task to execute
-					( const char * )"interpolate_matrix", 	// Name of the task, as a user friendly string
+					calculate_arrays,                  		// Function of the task to execute
+					( const char * )"calculate_arrays", 	// Name of the task, as a user friendly string
 					configMINIMAL_STACK_SIZE*2,   			// Stack quantity of the task
 					0,                            			// Task parameters
 					tskIDLE_PRIORITY+1,           			// Task priority
 					0                             			// Pointer to the task created in the system
 				);
 
+				row = 0;
+				xQueueSend( xSetIndexQueue, ( void * ) &row, ( TickType_t ) 0 );
 
 				vTaskDelete(TaskHandle_set_matrix_index);
 				vTaskDelete(NULL);
@@ -120,7 +126,7 @@ void get_pressure_value( void* pvParameters )
 						xQueueSendToBack( xPrintQueue, ( void * ) &int_zero, ( TickType_t ) 10 ); // Row for interpolation
 					}
 					#ifdef SENSOR_TEST
-					sensor_value = 7;
+					sensor_value = 5;
 					#endif
 				}
 			}
@@ -134,18 +140,14 @@ void get_pressure_value( void* pvParameters )
 	}
 }
 
-
-void interpolate_matrix( void* pvParameters)
+void calculate_arrays( void* pvParameters)
 {
 	TickType_t xPeriodicity =  1000 / portTICK_RATE_MS;
 	TickType_t xLastWakeTime = xTaskGetTickCount();
 
-	int row = 0;
+	int row;
 	int col = 0;
-
-	int aux_array_a[(MAX_COL*2)-1] = {};
-	int aux_array_b[(MAX_COL*2)-1] = {};
-	int aux_array_c[(MAX_COL*2)-1] = {};
+	int index[2];
 
 	bool_t interpol = false;
 
@@ -155,6 +157,11 @@ void interpolate_matrix( void* pvParameters)
 	// ---------- REPEAT FOR EVER --------------------------
 	while( TRUE )
 	{
+		if(xQueueReceive( xSetIndexQueue, &( row ), ( TickType_t ) 10 ) == pdTRUE)
+		{
+			//Get row
+		}
+
 		// First row of matrix:
 		if (row == 0)
 		{
@@ -169,104 +176,150 @@ void interpolate_matrix( void* pvParameters)
 		} else{ // Other rows
 
 			// Get row values:
-			if (interpol == false)
+			if (row % 2 == 0 ) // # Measurement row
 			{
-				if (row % 2 == 0 ) // # Measurement row
+				xQueueReceive( xPrintQueue, &( val ), ( TickType_t ) 10 );
+				aux_array_b[col] = val;
+				col ++;
+				if (col == ((MAX_COL*2)-1))
 				{
-					xQueueReceive( xPrintQueue, &( val ), ( TickType_t ) 10 );
-					aux_array_b[col] = val;
-					col ++;
-					if (col == ((MAX_COL*2)-1))
-					{
-						interpol = true;
-						row ++;
-						col = 0;
-					}
-
-				}else{ // Zeros row
-					xQueueReceive( xPrintQueue, &( val ), ( TickType_t ) 10 );
-					aux_array_c[col] = val;
-					col ++;
-					if (col == ((MAX_COL*2)-1))
-					{
-						row ++;
-						col = 0;
-					}
-				}
-			}else{ //Calculate interpolation
-
-				i = 0;
-				while(i != MAX_COL-1)
-				{
-					// Interpolation 1:
-					aux_array_c[(i*2)+1] = (aux_array_a[i*2] + aux_array_a[(i*2)+2] + aux_array_b[i*2] + aux_array_b[(i*2)+2])/4;
-
-					// Interpolation 2:
-					if (i != 0){
-                    	aux_array_c[(i*2)] = (aux_array_a[i*2] + aux_array_b[i*2] + aux_array_c[(i*2)+1] + aux_array_c[(i*2)])/4;
-					}else{
-                    	// First col:
-                    	aux_array_c[(i*2)] = (aux_array_a[i*2] + aux_array_b[i*2] + aux_array_c[(i*2)+1] )/3;}
-
-					if (row != 3){
-                    	aux_array_a[(i*2)+1] = (aux_array_a[i*2] + aux_array_a[(i*2)+2] + aux_array_c[(i*2)+1] + aux_array_a[(i*2)+1])/4;
-					}else{
-						// First row:
-						aux_array_a[(i*2)+1] = (aux_array_a[i*2] + aux_array_a[(i*2)+2] + aux_array_c[(i*2)+1])/3;}
-
-					if (row != (MAX_ROW*2)-1 ){
-						aux_array_b[(i*2)+1] = aux_array_c[(i*2)+1];
-					}else{
-						// Last row:
-						aux_array_b[(i*2)+1] = (aux_array_b[(i*2)] + aux_array_b[(i*2)+2] + aux_array_c[(i*2)+1])/3;}
-
-					if (i != (MAX_COL -2)){
-						// Set for next iteration:
-						aux_array_c[(i*2)+2] = aux_array_c[(i*2)+1];
-					}else{
-						// Last col:
-						aux_array_c[(i*2)+2] = (aux_array_a[(i*2)+2] + aux_array_b[(i*2)+2] + aux_array_c[(i*2)+1])/3;}
-
-					i++;
-				}
-
-				// Enqueue again:
-				i = 0;
-				for (i = 0; i < ((MAX_COL*2)-1); i++)
-				{
-					xQueueSend( xPrintQueue, ( void * ) &aux_array_a[i], ( TickType_t ) 0 );
-				}
-
-				for (i = 0; i < ((MAX_COL*2)-1); i++)
-				{
-					xQueueSend( xPrintQueue, ( void * ) &aux_array_c[i], ( TickType_t ) 0 );
-				}
-
-				for (i = 0; i < (sizeof(aux_array_a)/sizeof(aux_array_a[0])); i++)
-				{
-					aux_array_a[i] = aux_array_b[i];
-				}
-
-				interpol = false;
-				if (row == ((MAX_COL*2)-1))
-				{
-					for (i = 0; i < ((MAX_COL*2)-1); i++)
-					{
-						xQueueSend( xPrintQueue, ( void * ) &aux_array_b[i], ( TickType_t ) 0 );
-					}
 
 					xTaskCreate(
-							print_matrix,                  	// Function of the task to execute
-						( const char * )"print_matrix", 	// Name of the task, as a user friendly string
-						configMINIMAL_STACK_SIZE*2,   		// Stack quantity of the task
-						0,                            		// Task parameters
-						tskIDLE_PRIORITY+1,           		// Task priority
-						0                          			// Pointer to the task created in the system
+						interpolate_matrix,                  	// Function of the task to execute
+						( const char * )"interpolate_matrix", 	// Name of the task, as a user friendly string
+						configMINIMAL_STACK_SIZE*2,   			// Stack quantity of the task
+						0,                            			// Task parameters
+						tskIDLE_PRIORITY+1,           			// Task priority
+						0                             			// Pointer to the task created in the system
 					);
 
+					row ++;
+					xQueueSend( xSetIndexQueue, ( void * ) &row, ( TickType_t ) 0 );
 					vTaskDelete(NULL);
 				}
+
+			}else{ // Zeros row
+				xQueueReceive( xPrintQueue, &( val ), ( TickType_t ) 10 );
+				aux_array_c[col] = val;
+				col ++;
+				if (col == ((MAX_COL*2)-1))
+				{
+					row ++;
+					col = 0;
+				}
 			}
+		}
+	}
+}
+
+void interpolate_matrix( void* pvParameters)
+{
+	TickType_t xPeriodicity =  1000 / portTICK_RATE_MS;
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+
+	int row = 0;
+	int col = 0;
+
+	bool_t interpol = false;
+
+	int i = 0;
+	int j = 0;
+	int val;
+
+	stdioPrintf(UART_USED, "interpolate_matrix\n");
+
+	// ---------- REPEAT FOR EVER --------------------------
+	while( TRUE )
+	{
+
+		if(xQueueReceive( xSetIndexQueue, &( row ), ( TickType_t ) 10 ) == pdTRUE)
+		{
+			// Get row value
+		}
+
+		// Interpolation 1:
+		aux_array_c[(i*2)+1] = (aux_array_a[i*2] + aux_array_a[(i*2)+2] + aux_array_b[i*2] + aux_array_b[(i*2)+2])/4;
+
+		// Interpolation 2:
+		if (i != 0){
+			aux_array_c[(i*2)] = (aux_array_a[i*2] + aux_array_b[i*2] + aux_array_c[(i*2)+1] + aux_array_c[(i*2)])/4;
+		}else{
+			// First col:
+			aux_array_c[(i*2)] = (aux_array_a[i*2] + aux_array_b[i*2] + aux_array_c[(i*2)+1] )/3;}
+
+		if (row != 3){
+			aux_array_a[(i*2)+1] = (aux_array_a[i*2] + aux_array_a[(i*2)+2] + aux_array_c[(i*2)+1] + aux_array_a[(i*2)+1])/4;
+		}else{
+			// First row:
+			aux_array_a[(i*2)+1] = (aux_array_a[i*2] + aux_array_a[(i*2)+2] + aux_array_c[(i*2)+1])/3;}
+
+		if (row != (MAX_ROW*2)-1 ){
+			aux_array_b[(i*2)+1] = aux_array_c[(i*2)+1];
+		}else{
+			// Last row:
+			aux_array_b[(i*2)+1] = (aux_array_b[(i*2)] + aux_array_b[(i*2)+2] + aux_array_c[(i*2)+1])/3;}
+
+		if (i != (MAX_COL -2)){
+			// Set for next iteration:
+			aux_array_c[(i*2)+2] = aux_array_c[(i*2)+1];
+		}else{
+			// Last col:
+			aux_array_c[(i*2)+2] = (aux_array_a[(i*2)+2] + aux_array_b[(i*2)+2] + aux_array_c[(i*2)+1])/3;}
+
+		i++;
+
+		if (i == MAX_COL-1)
+		{
+			// Enqueue array a:
+			for (j = 0; j < ((MAX_COL*2)-1); j++)
+			{
+				xQueueSendToBack( xPrintQueue, ( void * ) &aux_array_a[j], ( TickType_t ) 10 ); // Enqueue matrix data
+			}
+
+			// Enqueue array a:
+			for (j = 0; j < ((MAX_COL*2)-1); j++)
+			{
+				xQueueSendToBack( xPrintQueue, ( void * ) &aux_array_c[j], ( TickType_t ) 10 );
+			}
+
+			// Copy array b to a:
+			for (j = 0; j < (sizeof(aux_array_a)/sizeof(aux_array_a[0])); j++)
+			{
+				aux_array_a[j] = aux_array_b[j];
+			}
+
+			interpol = false;
+			if (row == ((MAX_COL*2)-1))
+			{
+				for (j = 0; j < ((MAX_COL*2)-1); j++)
+				{
+					xQueueSendToBack( xPrintQueue, ( void * ) &aux_array_b[j], ( TickType_t ) 10 );
+				}
+
+				xTaskCreate(
+						print_matrix,                  	// Function of the task to execute
+					( const char * )"print_matrix", 	// Name of the task, as a user friendly string
+					configMINIMAL_STACK_SIZE*2,   		// Stack quantity of the task
+					0,                            		// Task parameters
+					tskIDLE_PRIORITY+1,           		// Task priority
+					0                          			// Pointer to the task created in the system
+				);
+
+				vTaskDelete(NULL);
+			}
+
+			xTaskCreate(
+				calculate_arrays,                  	// Function of the task to execute
+				( const char * )"calculate_arrays", // Name of the task, as a user friendly string
+				configMINIMAL_STACK_SIZE*2,   		// Stack quantity of the task
+				0,                            		// Task parameters
+				tskIDLE_PRIORITY+1,           		// Task priority
+				0                          			// Pointer to the task created in the system
+			);
+
+			xQueueSend( xSetIndexQueue, ( void * ) &row, ( TickType_t ) 0 );
+			vTaskDelete(NULL);
+
 		}
 	}
 }
@@ -281,7 +334,9 @@ void print_matrix( void* pvParameters )
 	int matrix_val;
 	int row=0, col=0;
 
-	stdioPrintf(UART_USED, "[");
+	stdioPrintf(UART_USED, "print_matrix\n");
+
+	stdioPrintf(UART_USED, ">[");
 	// ---------- REPEAT FOR EVER --------------------------
 	while( TRUE )
 	{
@@ -297,13 +352,17 @@ void print_matrix( void* pvParameters )
 				row++;
 				if (row < ((MAX_ROW*2)-1))
 				{
+					#ifdef SENSOR_TEST
 					stdioPrintf(UART_USED, "%d;\n", matrix_val);
+					#else
+					stdioPrintf(UART_USED, "%d;", matrix_val);
+					#endif
 				}else{
 					stdioPrintf(UART_USED, "%d", matrix_val);
 				}
 			}
 		}else{
-			stdioPrintf(UART_USED, "]\n\n");
+			stdioPrintf(UART_USED, "]<\n");
 
 			xTaskCreate(
 				set_matrix_index,                  		// Function of the task to execute
@@ -323,6 +382,7 @@ void print_matrix( void* pvParameters )
 				0                             			// Pointer to the task created in the system
 		   );
 
+		   row = 0;
 		   xQueueSend( xSetIndexQueue, ( void * ) &row, ( TickType_t ) 0 );
 
 			vTaskDelete(NULL);
